19b223e5b35ffabdd6de45f3f070344e40430dda
Heather Mccartney <heather.mccartney@anaplan.com>
2021-10-14 22:30:40 +0100

    Lint tests

--- day4/test_day4.py
+++ day4/test_day4.py
@@ -1,93 +1,95 @@
 from day4 import day4
 
+
 def test_is_valid_byr_too_low():
-    assert day4.is_valid_byr(1919) == False
+    assert day4.is_valid_byr(1919) is False
 
 
 def test_is_valid_byr_too_high():
-    assert day4.is_valid_byr(2003) == False
+    assert day4.is_valid_byr(2003) is False
 
 
 def test_is_valid_byr_just_right():
-    assert day4.is_valid_byr(1920) == True
-    assert day4.is_valid_byr(2002) == True
+    assert day4.is_valid_byr(1920) is True
+    assert day4.is_valid_byr(2002) is True
 
 
 def test_is_valid_iyr_too_low():
-    assert day4.is_valid_iyr(2009) == False
+    assert day4.is_valid_iyr(2009) is False
 
 
 def test_is_valid_iyr_too_high():
-    assert day4.is_valid_byr(2021) == False
+    assert day4.is_valid_byr(2021) is False
 
 
 def test_is_valid_iyr_just_right():
-    assert day4.is_valid_iyr(2010) == True
-    assert day4.is_valid_iyr(2020) == True
+    assert day4.is_valid_iyr(2010) is True
+    assert day4.is_valid_iyr(2020) is True
 
 
 def test_is_valid_eyr_too_low():
-    assert day4.is_valid_eyr(2019) == False
+    assert day4.is_valid_eyr(2019) is False
 
 
 def test_is_valid_eyr_too_high():
-    assert day4.is_valid_eyr(2031) == False
+    assert day4.is_valid_eyr(2031) is False
 
 
 def test_is_valid_eyr_just_right():
-    assert day4.is_valid_eyr(2020) == True
-    assert day4.is_valid_eyr(2030) == True
+    assert day4.is_valid_eyr(2020) is True
+    assert day4.is_valid_eyr(2030) is True
 
 
 def test_is_valid_height_cm_too_low():
-    assert day4.is_valid_hgt('149cm') == False
+    assert day4.is_valid_hgt('149cm') is False
 
 
 def test_is_valid_height_cm_too_high():
-    assert day4.is_valid_hgt('194cm') == False
+    assert day4.is_valid_hgt('194cm') is False
 
 
 def test_is_valid_height_cm_just_right():
-    assert day4.is_valid_hgt('150cm') == True
-    assert day4.is_valid_hgt('193cm') == True
+    assert day4.is_valid_hgt('150cm') is True
+    assert day4.is_valid_hgt('193cm') is True
 
 
 def test_is_valid_height_in_too_low():
-    assert day4.is_valid_hgt('58in') == False
+    assert day4.is_valid_hgt('58in') is False
 
 
 def test_is_valid_height_in_too_high():
-    assert day4.is_valid_hgt('77in') == False
+    assert day4.is_valid_hgt('77in') is False
 
 
 def test_is_valid_height_in_just_right():
-    assert day4.is_valid_hgt('59in') == True
-    assert day4.is_valid_hgt('76in') == True
+    assert day4.is_valid_hgt('59in') is True
+    assert day4.is_valid_hgt('76in') is True
+
 
 def test_is_valid_height_wrong_units():
-    assert day4.is_valid_hgt('5ft') == False
+    assert day4.is_valid_hgt('5ft') is False
 
 
 def test_is_valid_hcl_false():
-    assert day4.is_valid_hcl('#123abz') == False
-    assert day4.is_valid_hcl('123abc') == False
+    assert day4.is_valid_hcl('#123abz') is False
+    assert day4.is_valid_hcl('123abc') is False
 
 
 def test_is_valid_hcl_true():
-    assert day4.is_valid_hcl('#123abc') == True
+    assert day4.is_valid_hcl('#123abc') is True
 
 
 def test_is_valid_ecl_false():
-    assert day4.is_valid_ecl('wat') == False
+    assert day4.is_valid_ecl('wat') is False
 
 
 def test_is_valid_ecl_true():
-    assert day4.is_valid_ecl('brn') == True
+    assert day4.is_valid_ecl('brn') is True
 
 
 def test_is_valid_pid_false():
-    assert day4.is_valid_pid('0123456789') == False
+    assert day4.is_valid_pid('0123456789') is False
 
 
 def test_is_valid_pid_true():
-    assert day4.is_valid_pid('000000001') == True
+    assert day4.is_valid_pid('000000001') is True
