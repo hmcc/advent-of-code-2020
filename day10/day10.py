0032f8eb1469cc8becc052ec47194275037479bf
Heather Mccartney <heather.mccartney@anaplan.com>
2021-11-11 22:55:40 +0000

    DFS for part 2, not fast enough

--- day10/day10.py
+++ day10/day10.py
@@ -1,7 +1,3 @@
-from collections import defaultdict
-from functools import reduce
-
-
 def read_input(filename):
     values = []
     with open(filename) as file:
@@ -10,25 +6,24 @@ def read_input(filename):
     return values
 
 
-def diff(a, b):
-    return b - a
-
-
-def tally(a, b):
-    a[b] += 1
-    return a
+def paths(values, path_so_far, idx):
+    for offset in range(1, 4):
+        if idx + offset == len(values) - 1:
+            if values[idx + offset] - values[idx] < 4:
+                yield path_so_far + [values[-1]]
+            break
+        elif values[idx + offset] - values[idx] < 4:
+            for path in paths(values, path_so_far + [values[idx+offset]], idx+offset):
+                yield path
 
 
 # sort the adapters from the bag
-input_values = read_input('day10/input')
+input_values = read_input('day10/input_sample')
 input_values.sort()
 
 # add the charging outlet and the device
 input_values.insert(0, 0)
 input_values.append(input_values[-1] + 3)
 
-# find the difference between each pair, and tally them up
-differences = map(diff, input_values[:-1], input_values[1:])
-counts = reduce(tally, differences, defaultdict(int))
-
-print(counts)
+possible_paths = paths(input_values, input_values[:1], 0)
+print(sum(1 for ignore in possible_paths))
